bbd0c2d7c4ae8d49bab76ed704a7eeb82a799ef1
jlenon7 <lenonSec7@gmail.com>
2024-01-12 01:10:57 +0000

    fix: use right path of pm2 script

--- bin/pm2.js
+++ bin/pm2.js
@@ -2,7 +2,7 @@ export default {
   apps: [
     {
       name: 'api',
-      script: './bin/main.js',
+      script: './main.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true
