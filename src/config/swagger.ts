b7cc7088e50439a1354bd7b88fd6fc6567fb2aae
jlenon7 <lenonSec7@gmail.com>
2024-01-14 13:41:24 +0000

    fix(swagger): return json items in index route

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -4,6 +4,20 @@ export default {
       200: {
         description: 'Return multiple summoners profiles.',
         type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            region: { type: 'string' },
+            nickname: { type: 'string' },
+            summonerId: { type: 'string' },
+            accountId: { type: 'string' },
+            puuid: { type: 'string' },
+            createdAt: { type: 'string' },
+            updatedAt: { type: 'string' },
+            deletedAt: { type: 'string' }
+          }
+        },
         properties: {
           id: { type: 'string' },
           region: { type: 'string' },
